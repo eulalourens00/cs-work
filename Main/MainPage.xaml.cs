af1389b34c66bb5b25d11309d243c3375e16934a
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-09-09 14:31:32 +0300

    sql connect

---
+++ Main/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace Main
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition(),
+                    new RowDefinition()
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition(),
+                    new ColumnDefinition(),
+                    new ColumnDefinition()
+                }
+            };
+
+            grid.Add(new BoxView { Color = Color.FromRgb(25, 235, 25) }, 0, 0);
+            grid.Add(new BoxView { Color = Color.FromRgba(244, 201, 223, 122) }, 0, 1);
+            grid.Add(new BoxView { Color = Color.FromRgba(55, 225, 215, 132) }, 1, 0);
+
+            grid.Add(new BoxView { Color = Color.FromRgba(111, 211, 215, 24) }, 1, 1);
+            grid.Add(new BoxView { Color = Color.FromRgba(14, 122, 132, 54) }, 2, 0);
+            grid.Add(new BoxView { Color = Color.FromRgba(209, 202, 234, 12) }, 2, 1);
+
+            Content = grid;
+
+            //InitializeComponent();
+        }
+       
+    }
+
+}
---
+++ ado.net начало/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+namespace ado.net_начало
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"here need a path to Data Base";
+            string sqlexp = "";
+            using (SqlConnection sql = new SqlConnection(path))
+            {
+                sql.Open();
+                    //Console.WriteLine("Enter ur name: ");
+                    //string name = Console.ReadLine();
+                    
+
+                SqlCommand cmd = new SqlCommand();
+                //cmd.CommandText = "CREATE table Users";
+
+                    //cmd.CommandText = "Insert into Users(name) Values('{name}')";
+
+                cmd.CommandText = "Select * From Users";
+                    
+                cmd.Connection = sql;
+
+                //SqlCommand cmd = new SqlCommand(sqlexp, sql);
+                cmd.ExecuteNonQuery();
+                // cmd.ExecuteReader() - SELECT 
+                // cmd.ExecuteScalar() - MIN MAX СКАЛЯРНЫЕ ВЫРАЖЕНИЯ
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    string col1 = reader.GetName(0);
+                    string col2 = reader.GetName(1);
+                    Console.WriteLine($"{col1}\t{col2}");
+                }
+                while (reader.Read()) { 
+                    object id = reader.GetValue(0);
+                    object name = reader.GetValue(1);
+
+                    Console.WriteLine($"{id}\t{name}");
+                }
+                reader.Close();
+            }
+        }
+    }
+}
