a9e7bfe4256aaad4c76614a56ea137b5debdea41
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-09-11 17:32:21 +0300

    кнопочки

--- Main/MainPage.xaml.cs
+++ Main/MainPage.xaml.cs
@@ -4,34 +4,43 @@
     {
         public MainPage()
         {
-            Grid grid = new Grid
-            {
-                RowDefinitions =
-                {
-                    new RowDefinition(),
-                    new RowDefinition()
-                },
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition(),
-                    new ColumnDefinition(),
-                    new ColumnDefinition()
-                }
-            };
+            //Grid grid = new Grid
+            //{
+            //    RowDefinitions =
+            //    {
+            //        new RowDefinition(),
+            //        new RowDefinition()
+            //    },
+            //    ColumnDefinitions =
+            //    {
+            //        new ColumnDefinition(),
+            //        new ColumnDefinition(),
+            //        new ColumnDefinition()
+            //    }
+            //};
 
-            grid.Add(new BoxView { Color = Color.FromRgb(25, 235, 25) }, 0, 0);
-            grid.Add(new BoxView { Color = Color.FromRgba(244, 201, 223, 122) }, 0, 1);
-            grid.Add(new BoxView { Color = Color.FromRgba(55, 225, 215, 132) }, 1, 0);
+            //grid.Add(new BoxView { Color = Color.FromRgb(25, 235, 25) }, 0, 0);
+            //grid.Add(new BoxView { Color = Color.FromRgba(244, 201, 223, 122) }, 0, 1);
+            //grid.Add(new BoxView { Color = Color.FromRgba(55, 225, 215, 132) }, 1, 0);
 
-            grid.Add(new BoxView { Color = Color.FromRgba(111, 211, 215, 24) }, 1, 1);
-            grid.Add(new BoxView { Color = Color.FromRgba(14, 122, 132, 54) }, 2, 0);
-            grid.Add(new BoxView { Color = Color.FromRgba(209, 202, 234, 12) }, 2, 1);
+            //grid.Add(new BoxView { Color = Color.FromRgba(111, 211, 215, 24) }, 1, 1);
+            //grid.Add(new BoxView { Color = Color.FromRgba(14, 122, 132, 54) }, 2, 0);
+            //grid.Add(new BoxView { Color = Color.FromRgba(209, 202, 234, 12) }, 2, 1);
 
-            Content = grid;
+            //Content = grid;
 
-            //InitializeComponent();
+            InitializeComponent();
         }
-       
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            label1.Text= entry1.Text;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            label1.TextColor = Color.FromRgba(23, 124, 233, 0.3);
+        }
     }
 
 }
