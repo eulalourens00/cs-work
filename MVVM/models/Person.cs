81ebe0e5c19e8a5669dd8c58857bdaf990f50190
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-11-12 17:39:00 +0300

    11.11.2025 анкета сотрудников
    господи, спаси мою душу грешную

---
+++ MVVM/models/Person.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MVVM.models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _lastName;
+        private string _firstName;
+        private string _nameOfFather;
+        private string _gender;
+        private DateTime _birthday;
+        private string? _relationshipStatus;
+        private string? _extraInfo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string lastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(lastName));
+            }
+        }
+
+        public string firstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(firstName));
+            }
+        }
+
+        public string fatherName
+        {
+            get => _nameOfFather;
+            set
+            {
+                _nameOfFather = value;
+                OnPropertyChanged(nameof(fatherName));
+            }
+        }
+
+        public string gender
+        {
+            get => _gender;
+            set
+            {
+                _gender = value;
+                OnPropertyChanged(nameof(gender));
+            }
+        }
+
+        public DateTime birthday
+        {
+            get => _birthday;
+            set
+            {
+                _birthday = value;
+                OnPropertyChanged(nameof(birthday));
+            }
+        }
+
+        public string rel_status
+        {
+            get => _relationshipStatus;
+            set
+            {
+                _relationshipStatus = value;
+                OnPropertyChanged(nameof(rel_status));
+            }
+        }
+
+        public string extraInfo
+        {
+            get => _extraInfo;
+            set
+            {
+                _extraInfo = value;
+                OnPropertyChanged(nameof(extraInfo));
+            }
+        }
+    }
+}
+
---
+++ MVVM/viewModels/PersonVM.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Text.Json;
+using MVVM.models;
+
+namespace MVVM.viewModels
+{
+    class PersonVM:INotifyPropertyChanged
+    {
+        private Person person;
+        private string message;
+
+        // пошло поехало
+        public PersonVM()
+        {
+            Person = new Person
+            {
+                birthday = DateTime.Now.AddYears(-18)
+            };
+            SaveCommand = new Command(async () => await SavePersonAsync());
+        }
+
+        public Person Person
+        {
+            get => person;
+            set
+            {
+                person = value;
+                OnPropertyChanged(nameof(Person));
+            }
+        }
+        public string Message
+        {
+            get=> message;
+            set
+            {
+                Message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public List<string> Gender { get; } = new List<string>()
+        {
+            "Male", "Female"
+        };
+
+        public List<string> Relationship_Status { get; } = new List<string>()
+        {
+            "Single", "Married", "Brake Up", "Vdova"
+        };
+
+        public Command SaveCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task SavePersonAsync()
+        {
+            try
+            {
+                //господи, помоги
+                if (string.IsNullOrWhiteSpace(Person.firstName) || string.IsNullOrWhiteSpace(Person.firstName)
+                    || string.IsNullOrWhiteSpace(Person.fatherName) || string.IsNullOrWhiteSpace(Person.gender))
+                {
+                    message = "Add your data in space";
+                    return;
+                }
+
+                var personData = new
+                {
+                    Person.lastName,
+                    Person.firstName,
+                    Person.fatherName,
+                    Person.gender,
+                    birthday = Person.birthday.ToShortDateString(),
+                    Person.rel_status,
+                    Person.extraInfo,
+
+                    SavedAt = DateTime.Now.ToShortDateString()
+                };
+
+                string json = JsonSerializer.Serialize(personData, new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                });
+
+                string fileName = $"person_{DateTime.Now:yyyyMMdd_HHmmss}.json";
+                string filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+
+                await File.WriteAllTextAsync(filePath, json);
+
+                message = $"Saved at {fileName}"; //ура он прошел проверку ура
+
+                await Task.Delay(1000);
+                CleanForm();
+            }
+            catch (Exception ex) { message = $"Something's wrong.\n{ex.Message}"; };
+            // мамаа
+        }
+        public void CleanForm()
+        {
+            Person = new Person
+            {
+                birthday = DateTime.Now.AddYears(-18)
+            };
+            message = string.Empty;
+        }
+    }
+}
